275eb248ec4a956f4fececb96db96dc88fa4dca9
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-15 15:18:07 +0100

    feat: implement get all

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.use(express.urlencoded({ extended: true }))
+
+server.get('/api/accounts', async (req, res) => {
+  try {
+    const accounts = await db('accounts');
+    res.json(accounts);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get accounts' });
+  }
+});
+
 module.exports = server;
